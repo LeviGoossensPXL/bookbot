813c51e3e057d3a1ef9854b2a0bc79510317cccb
LeviGoossensPXL <levi.goossens107@gmail.com>
2025-07-31 18:19:37 +0200

    count number of words in text

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-    
+
+def get_num_of_words(text):
+    words_list = text.split()
+    return len(words_list)
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    book_text = get_book_text("books/frankenstein.txt")
+    # print(book_text)
+    num_words = get_num_of_words(book_text)
+    print(f"{num_words} words found in the document")
 
 main()
