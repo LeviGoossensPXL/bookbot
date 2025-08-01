5dcdf631d8c755321043dc974bb1fdc1c063d073
LeviGoossensPXL <levi.goossens107@gmail.com>
2025-08-01 08:58:17 +0200

    sort list of dictionaries

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from stats import get_num_of_words, get_num_of_letters
+from stats import get_num_of_words, get_num_of_letters, sort_dictionary
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
+def print_book_report():
+    
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
     # print(book_text)
     num_words = get_num_of_words(book_text)
     # print(f"{num_words} words found in the document")
     letter_counters = get_num_of_letters(book_text)
-    print(letter_counters)
+    # print(letter_counters)
+    print(sort_dictionary(letter_counters))
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def get_num_of_letters(text):
         else:
             counters[l] += 1
     return counters
+
+def sort_on_character(item):
+    return item["char"]
+
+def sort_dictionary(dictionary):
+    dictionary_list = []
+    for key in dictionary:
+        dictionary_list.append({"char": key, "num": dictionary[key]})
+    dictionary_list.sort(key=sort_on_character)
+    return dictionary_list
